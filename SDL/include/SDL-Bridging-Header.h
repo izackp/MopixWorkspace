c1bbccbd92b3328799bfbf6c6eed7c38dcbe5a97
Isaac Paul <izackp@gmail.com>
2022-06-16 18:28:11 -0400

    initial

---
+++ SDL/include/SDL-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  SDL-Bridging-Header.h
+//  SDL
+//
+//  Created by Isaac Paul on 4/11/22.
+//
+
+#ifndef SDL_Bridging_Header_h
+#define SDL_Bridging_Header_h
+
+
+#endif /* SDL_Bridging_Header_h */
